18ffcdc4dc3bd6ebb2b5090b88673a424f51795f
unknown <dongjooyoung@naver.com>
2023-05-02 01:46:43 +0900

    docs: 환경설정 및 데이터베이스 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoardModule } from './board/board.module';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './config/typeorm.config.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true}),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+      inject: [ConfigService]
+    }),
+    BoardModule, 
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
