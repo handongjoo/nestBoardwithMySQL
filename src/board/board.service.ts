26fa1e7c598c55223b6dffe9b35ef15eac724b4e
unknown <dongjooyoung@naver.com>
2023-05-02 03:14:45 +0900

    docs: 엔티티 및 레포지토리 작성

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Article } from 'src/entities/article.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class BoardService {}
+export class BoardService {
+    constructor(
+        @InjectRepository(Article) private articleRepository: Repository<Article>
+      ) {}
+}
---
+++ src/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity ({ schema: "board", name: "users"})
+export class User {
+    @PrimaryGeneratedColumn({type: "int", name: "id"})
+    id: number;
+
+    @Column('varchar', {length: 50})
+    email: string;
+
+    @Column('varchar', {length: 10})
+    name: string;
+
+    @Column('varchar', {length: 10})
+    password: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+  
+    @UpdateDateColumn()
+    updatedAt: Date;
+  
+    @DeleteDateColumn()
+    deletedAt: Date | null;
+}
